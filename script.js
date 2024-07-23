809da32e5cda17099ed773870c0538d6ba531796
Renan <renanpc.chaves@gmail.com>
2024-07-23 18:45:45 -0300

    :wrench: edit map

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-function initMap() {
-  // Create a map centered on a specific location
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 42.373004, lng: -71.110253 },
-    zoom: 18,
-  })
-
-  // Example of adding a marker
-  new google.maps.Marker({
-    position: { lat: 42.373004, lng: -71.110253 },
-    map: map,
-    title: "HUB",
-  })
-}
+let center = [-71.110253, 42.373004]
+const map = tt.map({
+  key: "XoPzIzeYZCt1Ql952oYxo30jK2cRgMFx",
+  container: "map",
+  center: center,
+  zoom: 16,
+})
+map.on("load", () => {
+  new tt.Marker().setLngLat(center).addTo(map)
+})
